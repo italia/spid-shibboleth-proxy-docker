fe8cb592507959278f5f7039e7f8016a7a9faeac
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-04-17 14:42:13 +0000

    Update /whoami index page

--- var/www/html/whoami/index.php
+++ var/www/html/whoami/index.php
@@ -1,11 +1,20 @@
 <html>
-<head>
-<title>Who Am I</title>
-</head>
-<body>
-<h1>WhoAmI</h1>
-<pre>
-<?php print_r($_SERVER); ?>
-</pre>
-</body>
+    <head>
+        <title>Who Am I</title>
+    </head>
+    <body>
+        <h1>WhoAmI</h1>
+
+        <h2>Server Environment</h2>
+        <pre><?php print_r($_SERVER); ?></pre>
+
+        <h2>Headers</h2>
+        <pre>
+        <?php
+            foreach (getallheaders() as $name => $value) {
+                echo "$name: $value\n";
+            }
+        ?>
+        </pre>
+    </body>
 </html>
