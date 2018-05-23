ca475fededa8ceb8d9584ffed9fbcd87d46569e0
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-05-23 10:53:52 +0000

    Add SPID smart button sources

---
+++ example/static/js/agid-spid-enter-config.js
@@ -0,0 +1,42 @@
+var config = {
+    "aruba": {
+        "url": "/login",
+        "title": "Accedi a SPID con Aruba",
+        "logo": "spid-idp-aruba.svg"
+    },
+    "infocert": {
+        "url": "/login",
+        "title": "Accedi a SPID con Infocert",
+        "logo": "spid-idp-infocertid.svg"
+    },
+    "poste": {
+        "url": "/login",
+        "title": "Accedi a SPID con Poste",
+        "logo": "spid-idp-posteid.svg"
+    },
+    "sielte": {
+        "url": "/login",
+        "title": "Accedi a SPID con Sielte",
+        "logo": "spid-idp-sielteid.svg"
+    },
+    "tim": {
+        "url": "/login",
+        "title": "Accedi a SPID con Tim",
+        "logo": "spid-idp-timid.svg"
+    },
+    "newidp1": {
+        "url": "/login",
+        "title": "Fornitore non attivo",
+        "logo": "spid-idp-dummy.svg"
+    },
+    "newidp2": {
+        "url": "/login",
+        "title": "Fornitore non attivo",
+        "logo": "spid-idp-dummy.svg"
+    },
+    "newidp3": {
+        "url": "/login",
+        "title": "Fornitore non attivo",
+        "logo": "spid-idp-dummy.svg"
+    }
+};
